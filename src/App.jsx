65ffaf814ef74f290e9341d0bd91a7f5596b167f
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-01-28 00:46:07 +0530

    Testimonials Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import AppStore from "./components/AppStore/AppStore";
 import Banner from "./components/Banner/Banner";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Services from "./components/Services/Services";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <Services />
       <Banner />
       <AppStore />
+      <Testimonials />
+      <Footer />
     </div>
   );
 };
